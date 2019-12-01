6e1d5f5f870d4e4f720119ec5e51ebbb46e8d0d9
MNFiT3 <mnfitv@gmail.com>
2019-12-01 18:11:10 +0800

    #3
    + Removed test route

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,6 +1,5 @@
 import { Router, Request, Response } from "express";
 import user from "./user";
-import test from "./test";
 import account from "./account";
 import admin from "./admin";
 import tolongbeli from "./tolongbeli";
@@ -8,7 +7,6 @@ import tolongbeli from "./tolongbeli";
 const routes = Router();
 
 routes.use("/user", user);
-routes.use("/test", test);
 routes.use("/account", account);
 routes.use("/admin", admin);
 routes.use("/tolongbeli", tolongbeli);
